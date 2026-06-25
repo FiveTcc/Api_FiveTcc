82697f7213a8c39112841abdc0fad5ca42e48fdc
Midiel <midielcastroramires400@gmail.com>
2026-06-24 22:41:19 -0300

    correcao -1

--- api_five-tcc/drizzle.config.ts
+++ api_five-tcc/drizzle.config.ts
@@ -1,11 +1,7 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-const url = `mysql://${process.env.DB_USER}:
-                     ${process.env.DB_PASSWORD}@
-                     ${process.env.DB_HOST}:
-                     ${process.env.DB_PORT}/
-                     ${process.env.DB_DATABASE}`;
+const url = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 export default defineConfig({
   dialect: 'mysql',
