80a847edac2680e39a434318faaad17efa6e4ecd
Brayan martins <brayanstilos@gmail.com>
2026-06-25 21:22:59 -0300

    atualizado

--- api_five-tcc/src/db/schemas/Ambiente.ts
+++ api_five-tcc/src/db/schemas/Ambiente.ts
@@ -2,25 +2,23 @@ import {
   mysqlTable,
   int,
   varchar,
-  text,
   boolean,
   timestamp,
+  
 } from 'drizzle-orm/mysql-core';
+import {User} from "./User";
 
-export const Ambiente = mysqlTable('ambientes', {
-  amb_id: int('amb_id').autoincrement().primaryKey(),
+export const ambientes = mysqlTable('ambientes', {
+  id_amb: int('id').autoincrement().primaryKey(),
+  amb_Nome: varchar('amb_nome', { length: 100 }).notNull(),
+  amb_Tipo: varchar('amb_tipo', { length: 50 }).notNull(),
+  amb_Bloco: varchar('amb_bloco', { length: 5 }).notNull(),
+  amb_Andar: varchar('amb_andar', { length: 2 }).notNull(),
+  amb_Ativo: boolean('amb_ativo').default(true).notNull(),
+  amb_Obs: varchar('amb_obs', { length: 100 }),
+  atualizado_em: timestamp("atualizado_em").defaultNow().onUpdateNow().notNull(),
+  criado_em: timestamp("criado_em").defaultNow().notNull(),
+  id_user: int('id_user').notNull().references(() => User.id_user)
 
-  amb_nome: varchar('amb_nome', { length: 100 }).notNull(),
 
-  amb_tipo: varchar('amb_tipo', { length: 50 }).notNull(),
-
-  amb_bloco: varchar('amb_bloco', { length: 50 }).notNull(),
-
-  amb_descricao: text('amb_descricao'),
-
-  amb_ativo: boolean('amb_ativo').default(true),
-
-  createdAt: timestamp('created_at').defaultNow(),
-
-  updatedAt: timestamp('updated_at').defaultNow(),
 });
